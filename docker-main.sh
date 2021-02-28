7854c22ba4f6e969fe4aa04b9db62a7c18166558
Joseph Vusich <15684831+josephvusich@users.noreply.github.com>
2021-02-28 21:05:16 +0000

    feat: Add check for missing/invalid config

--- docker-main.sh
+++ docker-main.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 
 . /etc/route53-ddns/config
+if [ -z "$DDNS_API_KEY" ]; then
+  echo "DDNS_API_KEY not specified."
+  exit 1
+fi
+if [ -z "$DDNS_SECRET" ]; then
+  echo "DDNS_SECRET not specified."
+  exit 1
+fi
+if [ -z "$DDNS_HOSTNAME" ]; then
+  echo "DDNS_HOSTNAME not specified."
+  exit 1
+fi
+if [ -z "$DDNS_URL" ]; then
+  echo "DDNS_URL not specified."
+  exit 1
+fi
+if [ -z "$DDNS_IPVERSIONS" ]; then
+  echo "DDNS_IPVERSIONS not specified."
+  exit 1
+fi
+
 while true; do
   for i in $(echo "$DDNS_IPVERSIONS" | sed "s/,/ /g"); do
     echo "Updating $i..."
